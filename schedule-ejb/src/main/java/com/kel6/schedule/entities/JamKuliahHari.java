04b9f6df23a43ac0874cabcf835444b69c3b2d4f
Noor_Falih <falih32@gmail.com>
2014-05-13 03:07:42 +0700

    add JamKuliahHari

---
+++ schedule-ejb/src/main/java/com/kel6/schedule/entities/JamKuliahHari.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kel6.schedule.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Junta
+ */
+@Entity
+@Table(name = "jam_kuliah_hari")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "JamKuliahHari.findAll", query = "SELECT j FROM JamKuliahHari j"),
+    @NamedQuery(name = "JamKuliahHari.findByIdJam", query = "SELECT j FROM JamKuliahHari j WHERE j.idJam = :idJam"),
+    @NamedQuery(name = "JamKuliahHari.findByWaktuJam", query = "SELECT j FROM JamKuliahHari j WHERE j.waktuJam = :waktuJam")})
+public class JamKuliahHari implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_jam")
+    private Long idJam;
+    @Size(max = 30)
+    @Column(name = "waktu_jam")
+    private String waktuJam;
+    @OneToMany(mappedBy = "idJam")
+    private Collection<SlotWaktu> slotWaktuCollection;
+    @OneToMany(mappedBy = "idJam")
+    private Collection<KetersediaanDosen> ketersediaanDosenCollection;
+
+    public JamKuliahHari() {
+    }
+
+    public JamKuliahHari(Long idJam) {
+        this.idJam = idJam;
+    }
+
+    public Long getIdJam() {
+        return idJam;
+    }
+
+    public void setIdJam(Long idJam) {
+        this.idJam = idJam;
+    }
+
+    public String getWaktuJam() {
+        return waktuJam;
+    }
+
+    public void setWaktuJam(String waktuJam) {
+        this.waktuJam = waktuJam;
+    }
+
+    @XmlTransient
+    public Collection<SlotWaktu> getSlotWaktuCollection() {
+        return slotWaktuCollection;
+    }
+
+    public void setSlotWaktuCollection(Collection<SlotWaktu> slotWaktuCollection) {
+        this.slotWaktuCollection = slotWaktuCollection;
+    }
+
+    @XmlTransient
+    public Collection<KetersediaanDosen> getKetersediaanDosenCollection() {
+        return ketersediaanDosenCollection;
+    }
+
+    public void setKetersediaanDosenCollection(Collection<KetersediaanDosen> ketersediaanDosenCollection) {
+        this.ketersediaanDosenCollection = ketersediaanDosenCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idJam != null ? idJam.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof JamKuliahHari)) {
+            return false;
+        }
+        JamKuliahHari other = (JamKuliahHari) object;
+        if ((this.idJam == null && other.idJam != null) || (this.idJam != null && !this.idJam.equals(other.idJam))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.kel6.schedule.entities.JamKuliahHari[ idJam=" + idJam + " ]";
+    }
+    
+}
